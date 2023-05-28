812cbc9b31d2b61692f8efed0722c9b4eef71807
João Pedro Lima Affonso de Carvalho <joaopolicarvalho99@usp.br>
2023-05-28 05:24:10 -0300

    Delete Projetos em Java/MavenStart directory

--- Projetos em Java/MavenStart/src/main/java/InitialTester.java
+++
@@ -1,9 +0,0 @@
-public abstract class TesteInt {
-
-    public static final SerializingManagable getInitSerializingManager(){
-
-        System.out.println("Início do Teste:!!!! ^^");
-        return new SerializingManager();
-
-    }
-}
